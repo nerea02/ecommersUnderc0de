7948dd81f277951b686fc24ad970236dcbea9392
daniguillen <72428160+daniguillen@users.noreply.github.com>
2024-10-08 21:37:55 -0300

    Ejemplo de base de data
    actualizacion de estado

--- routes/routes.js
+++ routes/routes.js
@@ -90,7 +90,7 @@ router.post('/nuevoNivel', async (req, res) => {
         });
     } catch (error) {
         console.error('Error al insertar nivel:', error);
-        return res.status(500).json({ message: 'Error al guardar el nivel', error: error.message });
+        return res.status(500).json({ message: 'Error al guardar el nivel', error: error});
     }
 });
 
@@ -1013,4 +1013,52 @@ router.get('/ClientesActivos', async (req, res) => {
     const [rows] = await pool.query('select * from clientes c  where c.Estado =1 ');
     res.json(rows);
 });
+
+
+
+
+
+
+//actualizar estado
+
+router.put('/BajaLogicaNivel', async (req, res) => {
+    const { id } = req.body; // Obtener el ID del nivel a actualizar
+   
+
+    try {
+        const query = 'UPDATE Niveles SET Estado = 0 WHERE IdNivel = ?';
+        const [result] = await pool.query(query, [ id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Nivel no encontrado" });
+        }
+
+        return res.status(200).json({ message: "Nivel actualizado exitosamente" });
+    } catch (error) {
+        console.error('Error al actualizar nivel:', error);
+        return res.status(500).json({ message: 'Error al actualizar nivel', error: error.message });
+    }
+});
+router.put('/AltaLogicaNivel', async (req, res) => {
+    const { id } = req.body; // Obtener el ID del nivel a actualizar
+   
+
+    try {
+        const query = 'UPDATE Niveles SET Estado = 1 WHERE IdNivel = ?';
+        const [result] = await pool.query(query, [ id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Nivel no encontrado" });
+        }
+
+        return res.status(200).json({ message: "Nivel actualizado exitosamente" });
+    } catch (error) {
+        console.error('Error al actualizar nivel:', error);
+        return res.status(500).json({ message: 'Error al actualizar nivel', error: error.message });
+    }
+});
+
+
+
+
 export default router;
