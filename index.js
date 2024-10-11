4bb818b4304e022c15b796ed9cd542f249026f2c
sotobel <marianabelencita29@gmail.com>
2024-10-11 18:04:52 -0300

    cors
    cors

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import express from 'express';
 import { PORT } from './config.js';
 import indexRouter from './routes/routes.js';
 import getRouter from './routes/getRouters.js';
+import cors from 'cors'; 
+
 const app = express();
 
-app.use(express.json());
+app.use(cors()); 
+app.use(express.json()); 
+
+// Definir rutas
 app.use('/api', indexRouter);
 app.use('/api', getRouter);
+
+// Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
 });
