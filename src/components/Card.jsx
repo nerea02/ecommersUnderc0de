acaa1888cbe364724a5597f73019b79106b0b439
daniguillen <72428160+daniguillen@users.noreply.github.com>
2024-10-11 22:04:03 -0300

    skeleton
    card de skeleton

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import Btn from "./Btn";
 import { useState, useEffect } from "react";
 import "./card.css";
+import ComponenteSkeleton from "./SkeletonCard";
 
 const Card = ({ image }) => {
   const [productos, setProductos] = useState([]); // Inicializa como array vacío
   const [error, setError] = useState(null); // Estado para errores
-
+  const [estadoApi, setEstadoApi] = useState(false); // Estado para errores
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -17,6 +18,7 @@ const Card = ({ image }) => {
         const data = await response.json();
         console.log(data);
         setProductos(data);
+        setEstadoApi(true);
       } catch (error) {
         console.log("error");
         setError(error.message);
@@ -26,28 +28,35 @@ const Card = ({ image }) => {
     fetchData(); // Llama a la función fetchData
   }, []);
   console.log(productos);
-
-  return (
-    <div className="row justify-content-evenly d-flex">
-      {productos.map((producto, i) => (
-        <div key={i} className="card col-3 m-2 d-flex align-items-sm-center">
-          <img
-            src={image}
-            alt={producto.nombre ? producto.nombre : ""}
-            className="image"
-          />
-          <div className="info align-items-sm-center d-flex flex-column">
-            <h2>{producto.Nombre ? producto.Nombre : ""}</h2>
-            <p className="price">${producto ? producto.Precio : ""}</p>
-            <p className="price">
-              {producto.Descripcion ? producto.Descripcion : ""}
-            </p>
-            <Btn Texto="Agregar al Carrito" />
+  if (!estadoApi) {
+    return (
+      <>
+        <ComponenteSkeleton />
+      </>
+    );
+  } else {
+    return (
+      <div className="row justify-content-evenly d-flex">
+        {productos.map((producto, i) => (
+          <div key={i} className="card col-3 m-2 d-flex align-items-sm-center">
+            <img
+              src={image}
+              alt={producto.nombre ? producto.nombre : ""}
+              className="image"
+            />
+            <div className="info align-items-sm-center d-flex flex-column">
+              <h2>{producto.Nombre ? producto.Nombre : ""}</h2>
+              <p className="price">${producto ? producto.Precio : ""}</p>
+              <p className="price">
+                {producto.Descripcion ? producto.Descripcion : ""}
+              </p>
+              <Btn Texto="Agregar al Carrito" />
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Card;
---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,33 @@
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+export default function ComponenteSkeleton() {
+  const skeletons = Array(6).fill(null); // Array con 6 elementos vacíos
+
+  return (
+    <div className="row justify-content-evenly d-flex">
+      {skeletons.map((_, index) => (
+        <div
+          key={index}
+          className="card col-3 m-2 d-flex align-items-sm-center"
+        >
+          <div className="image" style={{ width: "100%", height: "200px" }}>
+            <Skeleton width={"100%"} height={"100%"} />
+          </div>
+          <div className="info align-items-sm-center d-flex flex-column">
+            <h2>
+              <Skeleton width={150} height={30} />
+            </h2>
+            <p className="price">
+              <Skeleton width={100} height={20} />
+            </p>
+            <p className="price">
+              <Skeleton width={100} height={20} />
+            </p>
+            <Skeleton width={200} height={20} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
