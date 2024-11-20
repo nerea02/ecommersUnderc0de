54943310148d36695404c0dcdff1afb0fe2ec189
GlenBarbier <glendabarbiero@gmail.com>
2024-11-20 19:13:20 -0300

    ahora si lo abm
    elaborado por dani el crack

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Index from "./webs";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Index from "./webs";
+import AboutUs from "./webs/aboutUs.jsx";
+
 import Login from "./webs/login";
 import NewTrends from "./webs/newTrends.jsx";
-import AboutUs from "./webs/aboutUs.jsx";
-import Clientes from "./webs/Clientes/clientes";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route exact path="/Login" element={<Login />} />
           <Route exact path="/newTrends" element={<NewTrends />} />
           <Route exact path="/AboutUs" element={<AboutUs />} />
-          <Route exact path="/clientes" element={<Clientes />} />
+         
         </Routes>
       </BrowserRouter>
     </>
